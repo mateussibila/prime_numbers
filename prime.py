c3b4fb6a6b21e962e403d02c3f450a6092a0cfd6
mateussibila <mateussibila@gmail.com>
2024-08-31 14:24:17 +1000

    added prime.py

---
+++ prime.py
@@ -0,0 +1,24 @@
+import math
+
+
+def is_prime(n):
+    """calcula se o número é primo"""
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+
+def main():
+    """contém toda a lógica principal"""
+
+    for i in range(100):
+        if is_prime(i):
+            print(i, end=' ')
+    print()
+
+
+if __name__ == '__main__':
+    main()
